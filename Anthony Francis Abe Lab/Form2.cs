3bc0c81985f558f5b172b1b62ee2e9f2310d8dc5
itsmeYakult <144757385+ItsmeYakult14@users.noreply.github.com>
2024-09-03 13:23:47 +0800

    Add files via upload

---
+++ Anthony Francis Abe Lab/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Personalinfocs p)
+        {
+            InitializeComponent();
+            label6.Text = p.getName();
+            label7.Text = Convert.ToString(p.getAge());
+            label8.Text = Convert.ToString(p.getContact());
+            label9.Text = p.getAddress();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
